580a02ca07f2bc335e7b7927fcd1e65e452df2b1
Daniel <1001.27539960.ucla@gmail.com>
2024-11-29 16:31:14 -0400

    Update db.js
    password protected

--- Api/db.js
+++ Api/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');  // Usar la versión con promesas
 const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '1502Luis*',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'db',
     waitForConnections: true,  // Esperar por conexiones libres
     connectionLimit: 10,       // Límite de conexiones simultáneas
