8564acab60248022d7dccf6fd0cd3c698defc347
Daniel <1001.27539960.ucla@gmail.com>
2024-12-01 15:55:58 -0400

    todo correcto

--- Api/authMiddleware.js
+++ Api/authMiddleware.js
@@ -1,13 +1,17 @@
+const db = require('./db')
 const jwt = require('jsonwebtoken')
-
-const authenticateToken = (req, res, next) => {
+const authenticateToken = async (req, res, next) => {
   const authHeader = req.headers.authorization
   const token = authHeader && authHeader.split(' ')[1] // Extraer el token del encabezado
 
   if (!token) {
     return res.status(401).json({ error: 'Acceso denegado. Token no proporcionado.' })
   }
-
+  const query = 'SELECT * FROM revoked_tokens WHERE token = ?'
+  const [results] = await db.execute(query, [token])
+  if (results.length > 0) {
+    return res.status(401).json({ error: 'Token inválido.' })
+  }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET) // Verificar token
     req.user = decoded // Agregar usuario al objeto de solicitud
--- Api/db.js
+++ Api/db.js
@@ -1,15 +1,14 @@
-
-const mysql = require('mysql2/promise');  // Usar la versión con promesas
+const mysql = require('mysql2/promise') // Usar la versión con promesas
 
 // Crear un pool de conexiones
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'db',
-    waitForConnections: true,  // Esperar por conexiones libres
-    connectionLimit: 10,       // Límite de conexiones simultáneas
-    queueLimit: 0             // Sin límite en la cola
-});
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'db',
+  waitForConnections: true, // Esperar por conexiones libres
+  connectionLimit: 10, // Límite de conexiones simultáneas
+  queueLimit: 0 // Sin límite en la cola
+})
 
-module.exports = db;
+module.exports = db
